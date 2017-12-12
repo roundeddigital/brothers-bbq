5bf7fe4367a928d04d0604887e3c3c7da44fdf0e
Darren Hankins <hankins.darren@gmail.com>
2017-12-11 20:48:55 -0700

    updated functions.php to not charge processing and handling fees when only gift cards are being ordered

--- php/functions.php
+++ php/functions.php
@@ -24,61 +24,67 @@ add_filter( 'woocommerce_ship_to_different_address_checked','__return_false' );
  *
  * Add to theme functions.php
  */
-add_action( 'woocommerce_cart_calculate_fees','woocommerce_custom_surcharge' );
-function woocommerce_custom_surcharge() {
-  global $woocommerce;
-
-	if ( is_admin() && ! defined( 'DOING_AJAX' ) )
-		return;
-
-	$percentage = 0.04;
-	$surcharge = ( $woocommerce->cart->cart_contents_total + $woocommerce->cart->shipping_total ) * $percentage;
-	$woocommerce->cart->add_fee( 'Processing Fee', $surcharge, true, '' );
-
-}
+// add_action( 'woocommerce_cart_calculate_fees','woocommerce_custom_surcharge' );
+// function woocommerce_custom_surcharge() {
+//   global $woocommerce;
+//
+// 	if ( is_admin() && ! defined( 'DOING_AJAX' ) )
+// 		return;
+//
+// 	$percentage = 0.04;
+// 	$surcharge = ( $woocommerce->cart->cart_contents_total + $woocommerce->cart->shipping_total ) * $percentage;
+// 	$woocommerce->cart->add_fee( 'Processing Fee', $surcharge, true, '' );
+//
+// }
 
 add_action( 'woocommerce_cart_calculate_fees','wc_add_surcharge' );
 function wc_add_surcharge() {
-global $woocommerce;
+  global $woocommerce;
 
-if ( is_admin() && ! defined( 'DOING_AJAX' ) )
-return;
+  if ( is_admin() && ! defined( 'DOING_AJAX' ) )
+  return;
 
-// global $woocommerce;
-$total_items=0;
-$total_item_cards=0;
-$items = $woocommerce->cart->get_cart();
+  // global $woocommerce;
+  $total_items=0;
+  $total_item_cards=0;
+  $items = $woocommerce->cart->get_cart();
 
-foreach($items as $item => $values) {
-	$total_items=$total_items+1;
-	$card_id = $values['data']->get_id();
-	if ($card_id == 67295 || $card_id == 67298 || $card_id == 67299 || $card_id == 67300) {
-		// echo "Total Cards: ".$total_item_cards=($total_item_cards+1)."<br>";
-    $total_item_cards=$total_item_cards+1;
-	}
-	// echo "Total Items ".$total_items."<br>";
-	// echo $values['data']->get_id()."<br>";
-	// $_product =  wc_get_product( $values['data']->get_id());
-  // echo "<b>".$_product->get_title().'</b>  <br> Quantity: '.$values['quantity'].'<br>';
-	// echo "<b>".$_product->get_shipping_class().'</b><br>';
-  // echo "<b>".$_product->get_shipping_class_id().'</b><br>';
+  foreach($items as $item => $values) {
+  	$total_items=$total_items+1;
+  	$card_id = $values['data']->get_id();
+  	if ($card_id == 67295 || $card_id == 67298 || $card_id == 67299 || $card_id == 67300) {
+  		// echo "Total Cards: ".$total_item_cards=($total_item_cards+1)."<br>";
+      $total_item_cards=$total_item_cards+1;
+  	}
+  	// echo "Total Items ".$total_items."<br>";
+  	// echo $values['data']->get_id()."<br>";
+  	// $_product =  wc_get_product( $values['data']->get_id());
+    // echo "<b>".$_product->get_title().'</b>  <br> Quantity: '.$values['quantity'].'<br>';
+  	// echo "<b>".$_product->get_shipping_class().'</b><br>';
+    // echo "<b>".$_product->get_shipping_class_id().'</b><br>';
 
-  // $price = get_post_meta($values['product_id'] , '_price', true);
-  // echo "  Price: ".$price."<br>";
+    // $price = get_post_meta($values['product_id'] , '_price', true);
+    // echo "  Price: ".$price."<br>";
 
-  // echo "Flat Rates: ".  get_option("woocommerce_flat_rates");
-}
+    // echo "Flat Rates: ".  get_option("woocommerce_flat_rates");
+  }
 
+// Total items ordered is equal to total gift cards
+// meaning that there is no products being ordered
+// and there is no reason to charge processing and handling
 if ($total_items == $total_item_cards) {
 	$fee = 0.00;
 } else {
-	// change the $fee to set the surcharge
+  // card processing surcharge fee
+  $percentage = 0.04;
+  $surcharge = ( $woocommerce->cart->cart_contents_total + $woocommerce->cart->shipping_total ) * $percentage;
+  $woocommerce->cart->add_fee( 'Processing Fee', $surcharge, true, '' );
+
+	// handling surchange fee
 	$fee = 13.00;
-}
-
-$county = array('US');
-
-if ( in_array( WC()->customer->get_shipping_country(), $county ) ) :
-    $woocommerce->cart->add_fee( 'Handling Fee', $fee, true, 'standard' );
-endif;
+  $county = array('US');
+  if ( in_array( WC()->customer->get_shipping_country(), $county ) ) :
+      $woocommerce->cart->add_fee( 'Handling Fee', $fee, true, 'standard' );
+  endif;
+  }
 }
